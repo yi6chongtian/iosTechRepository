116ab96ba2b07745e2bdb42a9c52476c1202e492
tzb <tzb@yfyun.net>
2018-01-14 16:38:34 +0800

    初始化项目

---
+++ iosTechRepostitory/iosTechRepostitory/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iosTechRepostitory
+//
+//  Created by tang on 2018/1/14.
+//  Copyright © 2018年 tang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ iosTechRepostitory/iosTechRepostitory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iosTechRepostitory
+//
+//  Created by tang on 2018/1/14.
+//  Copyright © 2018年 tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
