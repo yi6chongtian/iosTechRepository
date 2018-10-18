2ac728dc4851785b3b3f527c602df24e8702e36c
tangzhibiao <tangzhibiao@aliyun.com>
2018-10-18 16:46:57 +0800

    图表

--- iosTechRepostitory/iosTechRepostitory/ViewController.m
+++ iosTechRepostitory/iosTechRepostitory/ViewController.m
@@ -11,6 +11,8 @@
 #import "BIDashBoard.h"
 #import "BITemperatureDashBoard.h"
 #import "BILineChart.h"
+#import "RectangleIndicatorView.h"
+#import "CircleIndicatorView.h"
 
 @interface ViewController ()
 
@@ -19,6 +21,10 @@
 @property (nonatomic,strong) BITemperatureDashBoard *tempDashView;
 
 @property (nonatomic,strong) BILineChart *lineChart;
+
+@property (strong, nonatomic)  CircleIndicatorView *circleIndicatorView;
+
+@property (strong, nonatomic)  RectangleIndicatorView *rectangleIndicatorView;
 
 @end
 
@@ -32,7 +38,7 @@
     dashView.dashBgColor = UIColor.blueColor;
     dashView.dashColor = UIColor.greenColor;
     dashView.lineWidth = 5;
-    dashView.frame = CGRectMake(100, 20, 35*2, 35);
+    dashView.frame = CGRectMake(100, 64, 35*2, 35);
     [self.view addSubview:dashView];
     self.dashView = dashView;
     
@@ -44,7 +50,7 @@
     
     NSArray *xData = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7"];
     NSArray *yData = @[@"1",@"2",@"3",@"4",@"5",@"6",@"5.5"];
-    BILineChart *lineChart = [[BILineChart alloc] initWithFrame:CGRectMake(35, CGRectGetMaxY(tempView.frame) + 10, self.view.frame.size.width - 30 - 8, 200)];
+    BILineChart *lineChart = [[BILineChart alloc] initWithFrame:CGRectMake(35, CGRectGetMaxY(tempView.frame) + 10, self.view.frame.size.width - 30 - 8, 100)];
     lineChart.displayDataPoint = YES;
     lineChart.bezierSmoothing = NO;
     lineChart.maxValue = 10;
@@ -62,6 +68,39 @@
     [lineChart setChartData:yData];
     [self.view addSubview:lineChart];
     self.lineChart = lineChart;
+    
+    self.rectangleIndicatorView = [[RectangleIndicatorView alloc] initWithFrame:CGRectMake(8, CGRectGetMaxY(lineChart.frame) + 20, self.view.frame.size.width - 8 - 8, 200)];
+    self.rectangleIndicatorView.backgroundColor = UIColor.whiteColor;
+    self.rectangleIndicatorView.minValue = 40;
+    self.rectangleIndicatorView.maxValue = 80;
+    self.rectangleIndicatorView.valueToShowArray = @[@40, @50, @60, @70, @80];
+    self.rectangleIndicatorView.indicatorValue = 50;
+    self.rectangleIndicatorView.minusBlock = ^{
+        NSLog(@"点击了 -");
+        self.rectangleIndicatorView.indicatorValue -= 1;
+    };
+    self.rectangleIndicatorView.addBlock = ^{
+        NSLog(@"点击了 +");
+        self.rectangleIndicatorView.indicatorValue += 1;
+    };
+    [self.view addSubview:self.rectangleIndicatorView];
+    
+    self.circleIndicatorView =  [[CircleIndicatorView alloc] initWithFrame:CGRectMake(8, CGRectGetMaxY(self.rectangleIndicatorView.frame) + 10, self.view.frame.size.width - 8 - 8, 80)];
+    self.circleIndicatorView.backgroundColor  = UIColor.whiteColor;
+    self.circleIndicatorView.minValue = 40;
+    self.circleIndicatorView.maxValue = 80;
+    self.circleIndicatorView.innerAnnulusValueToShowArray = @[@40, @50, @60, @70, @80];
+    self.circleIndicatorView.indicatorValue = 60;
+    self.circleIndicatorView.minusBlock = ^{
+        NSLog(@"点击了 -");
+        self.circleIndicatorView.indicatorValue -= 1;
+    };
+    self.circleIndicatorView.addBlock = ^{
+        NSLog(@"点击了 +");
+        self.circleIndicatorView.indicatorValue += 1;
+    };
+    [self.view addSubview:self.circleIndicatorView];
+
    
 }
 
